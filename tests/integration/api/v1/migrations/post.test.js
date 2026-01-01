339e6be1377112f93d8dd503bb6e21fa9c0eee78
Hugo Almeida <Hugo.Almeida@rhimagnesita.com>
2026-01-01 19:01:45 -0300

    refactoring of `database.js`, `get.test.js` and `post.test.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,10 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; create schema public;");
+}
+
 test("POST to /api/v1/migrations should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
