f385658a1b40efe271543d105443e15295828169
Hugo Almeida <Hugo.Almeida@rhimagnesita.com>
2026-02-05 11:38:46 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,12 +6,16 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public CASCADE; create schema public;");
 });
 
-test("GET to /api/v1/migrations should return status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations should return status 200", () => {
+  describe("Anonymous user", () => {
+    test("GET to /api/v1/migrations should return status 200", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
