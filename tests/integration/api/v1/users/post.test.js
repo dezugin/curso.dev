1285352dcb03b5095f52aa7d49e768ea6885d780
Hugo Almeida <Hugo.Almeida@rhimagnesita.com>
2026-03-17 12:00:15 -0300

    feat: create `user` model and `/api/v1/users` endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
+import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,16 +10,101 @@ beforeAll(async () => {
 describe("POST /api/v1/users should return status 200", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["DrDong", "drdong@example.com", "donghasahugedong"],
-      });
-      const users = await database.query("SELECT * FROM users");
-      console.log(users.rows);
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "DrDong",
+          email: "drdong@example.com",
+          password: "donghasahugedong",
+        }),
       });
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "DrDong",
+        email: "drdong@example.com",
+        password: "donghasahugedong",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+    test("With duplicated 'email'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedEmailUser1",
+          email: "duplicatedEmail@example.com",
+          password: "donghasahugedong",
+        }),
+      });
+      expect(response1.status).toBe(201);
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedEmailUser2",
+          email: "DuplicatedEmail@example.com",
+          password: "donghasahugedong",
+        }),
+      });
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "Email already exists",
+        action: "Please use a different email address",
+        statusCode: 400,
+      });
+    });
+    test("With duplicated 'username'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "duplicatedUsername",
+          email: "uniqueEmail@example.com",
+          password: "donghasahugedong",
+        }),
+      });
+      expect(response1.status).toBe(201);
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "DuplicatedUsername",
+          email: "anotherUniqueEmail@example.com",
+          password: "donghasahugedong",
+        }),
+      });
+      expect(response2.status).toBe(400);
+
+      const response2Body = await response2.json();
+
+      expect(response2Body).toEqual({
+        name: "ValidationError",
+        message: "Username already exists",
+        action: "Please use a different username",
+        statusCode: 400,
+      });
     });
   });
 });
