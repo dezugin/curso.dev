8a860ef42c5b174c9be16a89bea2ebd455f9742d
Hugo Almeida <Hugo.Almeida@rhimagnesita.com>
2026-03-17 11:59:18 -0300

    feat: create and use `ValidationError` in `cotroller.js` and `error.js`

--- infra/errors.js
+++ infra/errors.js
@@ -36,6 +36,25 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error occurred.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Adjust the Input values and try again.";
+    this.statusCode = 400;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method Not Allowed For this endpoint");
